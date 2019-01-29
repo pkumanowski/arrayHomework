5d8633c8431906ae327875834037b7f89c7df1c4
Paweł Kumanowski <pkumanowski@outlook.com>
2019-01-29 22:20:29 +0100

    Dynamiczne generowanie tablicy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int twoD [] [] = new int[5] [];
-        twoD[0] = new int[1];
-        twoD[1] = new int[2];
-        twoD[2] = new int[3];
-        twoD[3] = new int[4];
-        twoD[4] = new int[5];
+        System.out.println("Wpisz liczbę pierwszą tablicy dwuwymiarowej");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Wpisz liczbę drugą tablicy dwuwymiarowej");
+        int b = sc.nextInt();
+
+        int twoD [] [] = new int[a] [b];
 
         int i, j, k = 0;
-        for (i = 0; i < 5; i++)
+        for (i = 0; i < a; i++)
             for (j=0; j<i+1; j++){
                 twoD[i][j] = k;
                 k++;
             }
 
-        for (i=0; i < 5; i++){
+        for (i=0; i < a; i++){
             for(j=0; j<i+1; j++)
                 System.out.print(j + ", ");
             System.out.println();
